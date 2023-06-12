33ef0df6b944050572fa8377cc6ef4678d15670c
welitonlimaa <welitonlima159@gmail.com>
2023-06-12 18:45:32 -0300

    fix: change variable name jov to job (TESTE 2)

--- teste2.js
+++ teste2.js
@@ -1,17 +1,23 @@
-var data =  require("./fakeData");
+// mudança 1: utilizar "var" para declarar uma variável pode causar problemas pois "vaza o escopo"
+// mudei para "const"
+
+// mudança 2: a variável jov deveria ser job e assim foi alterada
+
+// mudança 3: para confirmar a adição a função sempre retorna o ultimo elemento adicionado em data
+
+const data =  require("./fakeData");
 
 module.exports = function(req, res){
   
-    var name =  req.body.name;
-    var jov =  req.body.job;
+    const name =  req.body.name;
+    const job =  req.body.job;
     
-    var newUser = {
-        name: name,
-        job: job,
+    const newUser = {
+        name,
+        job,
     }
 
     data.push(newUser)
     
-    res.send(newUser);
-
+    res.send(data[data.length - 1]);
 };
