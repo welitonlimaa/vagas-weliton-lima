046f26cbf70e66800569a6f5152c316948d87609
welitonlimaa <welitonlima159@gmail.com>
2023-06-12 19:26:27 -0300

    fix: change find HOF param (TESTE 4)

--- teste4.js
+++ teste4.js
@@ -1,12 +1,21 @@
-var data =  require("./fakeData");
+// mudança 1: utilizar "var" para declarar uma variável pode causar problemas pois "vaza o escopo"
+// mudei para "const"
+
+// mudança 2: correção no parâmetro passado para a HOF find asserção estava errada
+// o correto é (d.id == id)
+
+// mudança 3: desestruturação do body 
+
+const data =  require("./fakeData");
 
 module.exports =  function(req, res) {
   
-    var id =  req.query.id;
+    const id =  req.query.id;
+    const { name, job } = req.body;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    const reg = data.find(d => d.id == id);
+    reg.name = name;
+    reg.job = job;
 
     res.send(reg);
 
