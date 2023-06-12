ef05cb0e251faa925161bf9cbd8e73621e5161f9
welitonlimaa <welitonlima159@gmail.com>
2023-06-12 20:29:57 -0300

    refactor: teste 5 add response status and try catch

--- teste5.js
+++ teste5.js
@@ -3,10 +3,14 @@
 
 // mudança 2: conta as vezes em que o nome foi pesquisado com base nos registros de pesquisa
 
+// mudança 3: retorna o status da resposta a requisição
+
+// mudança 4: adiciona try catch para captura de erros no servidor
+
 const researchedNames =  require("./queriesRegistration");
 
 module.exports = function(req, res){
-    
+  try {
     const name =  req.query.name;
 
     const count = researchedNames.reduce((acc, curr) => { 
@@ -16,5 +20,9 @@ module.exports = function(req, res){
       return acc;
     }, 0)
 
-    res.send(`Usuário ${name} foi lido ${count} vezes.`);
-};
+    res.status(200).send(`Usuário ${name} foi lido ${count} vezes.`);
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};  
