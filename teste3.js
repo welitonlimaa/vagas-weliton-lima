3655a9fdfff5970dca359cdd5bed65f47f603ec0
welitonlimaa <welitonlima159@gmail.com>
2023-06-12 19:05:28 -0300

    refactor: delete function with find HOF and indexOf (TESTE 3)

--- teste3.js
+++ teste3.js
@@ -1,15 +1,19 @@
-var data =  require("./fakeData");
+// mudança 1: utilizar "var" para declarar uma variável pode causar problemas pois "vaza o escopo"
+// mudei para "const" ou "let"
+
+// mudança 2: a função pesquisa por um usuário que corresponda a query name e o "Deleta",
+// utilizar uma HOF nesse caso melhora a performance e a legibilidade do código
+
+const data =  require("./fakeData");
 
 module.exports = function(req, res) {
   
-    var name =  req.query.name;
+    const name =  req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
-    }
+    const user = data.find((data) => data.name == name);
+    const index = data.indexOf(user);
+
+    data[index] = null;
 
     res.send("success");
-
 };
