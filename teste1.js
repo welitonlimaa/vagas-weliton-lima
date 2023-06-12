df529a1d5b63131c84f97be73d0e795f3d7cc266
welitonlimaa <welitonlima159@gmail.com>
2023-06-12 20:09:50 -0300

    refactor: teste 1 add response status and try catch

--- teste1.js
+++ teste1.js
@@ -7,18 +7,30 @@ const researchedNames =  require("./queriesRegistration");
 // mudança 2: a função getUser pesquisa por um usuário que corresponda a query name,
 // utilizar uma HOF nesse caso melhora a performance e a legibilidade do código
 
-// mudança 2: registra a pesquisa do nome em researchedNames
+// mudança 3: registra a pesquisa do nome em researchedNames
+
+// mudança 4: retorna o status da requisição
+
+// mudança 5: adiciona try catch para captura de erros no servidor
 
 const getUser = ( req, res, _next ) => {
+  try {
     const name =  req.query.name;
     researchedNames.push({ name, date: Date.now() });
     const result = data.find((user) => user.name == name);
 
-    res.send(result);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 const getUsers = ( _req, res, _next ) => {
+  try {
     res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 module.exports = {
